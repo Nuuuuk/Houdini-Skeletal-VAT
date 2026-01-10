0727230c67ac188188c59abf2ccdded1be5440a5
Lei <leiwu05@outlook.com>
2026-01-09 20:01:56 -0500

    chore: add minimal compilable UE files

---
+++ Source/SkeletalVAT/SkeletalVAT.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkeletalVAT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkeletalVAT, "SkeletalVAT" );
